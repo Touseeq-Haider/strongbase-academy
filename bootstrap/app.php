823f03b452f7b231ca41be95f2fe5f3b8a598bda
Devs <haiderwaleed59@gmail.com>
2026-07-15 23:54:23 +0500

    Fix HTTPS trust proxy for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,17 +8,24 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'role' => RoleMiddleware::class,
         ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn(Request $request) => $request->is('api/*'),
+
+        // Render (aur is jaisi hosting services) HTTPS ko edge pe "terminate" karti hain
+        // aur app ko internally HTTP pe forward karti hain. Ye batata hai Laravel ko
+        // ke asal request HTTPS thi, taake forms/links sahi se https:// generate hon.
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
         );
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
     })->create();
